2c9ca4131c879eb8df2fdf4fd82076c4d66557ce
Henrique <hs.ccti@gmail.com>
2013-11-16 14:10:59 -0200

    1 commit

---
+++ src/javarmiscript/Cliente.java
@@ -0,0 +1,10 @@
+package javarmiscript;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Cliente extends Remote {
+
+	public void Exibir(String mensagem) throws RemoteException;
+
+}
---
+++ src/javarmiscript/ServidorImpl.java
@@ -0,0 +1,5 @@
+package javarmiscript;
+
+public class ServidorImpl {
+
+}
